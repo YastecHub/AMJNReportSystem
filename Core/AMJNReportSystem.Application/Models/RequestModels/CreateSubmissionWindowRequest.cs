ea1226e69529f52c45b2d7dfb558c10de4b6e092
AbdulWaheed MahmoodAhmad <abdulwaheedmahmoodahmad6@gmail.com>
2024-08-31 09:54:10 +0100

    Completed Submission Window Module

--- Core/AMJNReportSystem.Application/Models/RequestModels/CreateSubmissionWindowRequest.cs
+++ Core/AMJNReportSystem.Application/Models/RequestModels/CreateSubmissionWindowRequest.cs
@@ -12,5 +12,8 @@ namespace AMJNReportSystem.Application.Models.RequestModels
         public Guid ReportSubmissionId { get; set; }
         public DateTime StartingDate { get; set; }
         public DateTime EndingDate { get; set; }
-    }
+		public int Month { get; set; }
+		public int Year { get; set; }
+		public bool IsLocked { get; set; }
+	}
 }
