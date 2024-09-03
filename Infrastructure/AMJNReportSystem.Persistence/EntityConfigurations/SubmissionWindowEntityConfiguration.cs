24dc5cb71641c41bb759a3eb77fc276936aa0baf
AbdulWaheed MahmoodAhmad <abdulwaheedmahmoodahmad6@gmail.com>
2024-09-03 18:35:05 +0100

    Entity Config

---
+++ Infrastructure/AMJNReportSystem.Persistence/EntityConfigurations/SubmissionWindowEntityConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using AMJNReportSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMJNReportSystem.Persistence.EntityConfigurations
+{
+	internal sealed class SubmissionWindowConfiguration : IEntityTypeConfiguration<SubmissionWindow>
+	{
+		public void Configure(EntityTypeBuilder<SubmissionWindow> builder)
+		{
+			builder.ToTable("SubmissionWindows");
+
+			builder.HasKey(sw => sw.Id);
+
+			builder.Property(sw => sw.StartingDate)
+				   .IsRequired();
+
+			builder.Property(sw => sw.EndingDate)
+				   .IsRequired();
+
+			builder.Property(sw => sw.Month)
+				   .IsRequired();
+
+			builder.Property(sw => sw.Year)
+				   .IsRequired();
+
+			builder.Property(sw => sw.IsLocked)
+				   .IsRequired();
+
+			builder.HasOne(sw => sw.ReportType)
+				   .WithMany()
+				   .HasForeignKey(sw => sw.ReportTypeId)
+				   .OnDelete(DeleteBehavior.Restrict);
+
+			builder.HasQueryFilter(sw => !sw.IsDeleted);
+		}
+	}
+
+}
