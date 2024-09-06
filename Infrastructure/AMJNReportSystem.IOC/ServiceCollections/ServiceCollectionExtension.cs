8aa1d4abf2d996d3a1c4e9dc43e1443866602462
AREMU KAMAL <aremukamal111@gmail.com>
2024-09-06 11:27:18 +0100

    up

--- Infrastructure/AMJNReportSystem.IOC/ServiceCollections/ServiceCollectionExtension.cs
+++ Infrastructure/AMJNReportSystem.IOC/ServiceCollections/ServiceCollectionExtension.cs
@@ -49,10 +49,7 @@ namespace AMJNReportSystem.IOC.ServiceCollections
                 .AddScoped<IReportTypeRepository, ReportTypeRepository>()
                 .AddScoped<IReportSectionRepository, ReportSectionRepository>()
                 .AddScoped<IReportResponseRepository, ReportResponseRepository>()
-                .AddScoped<IQuestionRepository, QuestionRepository>()
-                .AddScoped<IQuestionOptionRepository, QuestionOptionRepository>()
-                .AddScoped<ISubmissionWindowRepository, SubmissionWindowRepository>();
-                .AddScoped<IQuestionOptionRepository, QuestionOptionRepository>();
+                .AddScoped<IQuestionRepository, QuestionRepository>();
         }
 
         public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration config)
