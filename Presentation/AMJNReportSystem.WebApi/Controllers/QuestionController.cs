dddf1dc31612cf392c31b335b959baea09541e36
Abdulhamiid <oyeboharemu@yahoo.com>
2024-10-09 19:31:05 +0100

    Updates

--- Presentation/AMJNReportSystem.WebApi/Controllers/QuestionController.cs
+++ Presentation/AMJNReportSystem.WebApi/Controllers/QuestionController.cs
@@ -23,8 +23,8 @@ namespace AMJNReportSystem.WebApi.Controllers
 		[ProducesResponseType(StatusCodes.Status409Conflict)]
 		[ProducesResponseType(StatusCodes.Status400BadRequest)]
 		[ProducesResponseType(StatusCodes.Status200OK)]
-		[HttpPost("Create Question")]
-		[OpenApiOperation("Create new question.", "")]
+		[HttpPost("create-question")]
+		[OpenApiOperation("create-question", "")]
 		public async Task<IActionResult> CreateQuestion([FromBody] CreateQuestionRequest model, [FromServices] IValidator<CreateQuestionRequest> validator)
 		{ 
 			var validationResult = await validator.ValidateAsync(model);
@@ -37,8 +37,8 @@ namespace AMJNReportSystem.WebApi.Controllers
 		[ProducesResponseType(StatusCodes.Status409Conflict)]
 		[ProducesResponseType(StatusCodes.Status400BadRequest)]
 		[ProducesResponseType(StatusCodes.Status200OK)]
-		[HttpPut("Update Question")]
-		[OpenApiOperation("Update question.", "")]
+		[HttpPut("update-question")]
+		[OpenApiOperation("update-question", "")]
 		public async Task<IActionResult> UpdateQuestion([FromBody] UpdateQuestionRequest model ,Guid id, [FromServices] IValidator<UpdateQuestionRequest> validator)
 		{
 			var validationResult = await validator.ValidateAsync(model);
@@ -51,8 +51,8 @@ namespace AMJNReportSystem.WebApi.Controllers
 		[ProducesResponseType(StatusCodes.Status409Conflict)]
 		[ProducesResponseType(StatusCodes.Status400BadRequest)]
 		[ProducesResponseType(StatusCodes.Status200OK)]
-		[HttpDelete("Delete Question")]
-		[OpenApiOperation("Delet question.", "")]
+		[HttpDelete("delete-question")]
+		[OpenApiOperation("delete-question", "Delet question")]
 		public async Task<IActionResult> DeleteQuestion([FromRoute]Guid id)
 		{
 			var reportTypeRequest = await _questionService.DeleteQuestion(id);
@@ -64,7 +64,7 @@ namespace AMJNReportSystem.WebApi.Controllers
 		[ProducesResponseType(StatusCodes.Status400BadRequest)]
 		[ProducesResponseType(StatusCodes.Status404NotFound)] 
 		[HttpGet("{id}")]
-		[OpenApiOperation("Get a specific question by id.", "")]
+		[OpenApiOperation("get-question-by-id", "Get a specific question by id.")]
 		public async Task<IActionResult> GetQuestion(Guid id)
 		{
 			if (id == Guid.Empty) 
@@ -74,7 +74,7 @@ namespace AMJNReportSystem.WebApi.Controllers
 		}
 
 		[ProducesResponseType(StatusCodes.Status200OK)]
-		[HttpGet]
+		[HttpGet("get-all-questions")]
 		[OpenApiOperation("Get list of all question.", "")]
 		public async Task<IActionResult> GetQuestions()  
 		{
@@ -83,8 +83,8 @@ namespace AMJNReportSystem.WebApi.Controllers
 		}
 
 		[ProducesResponseType(StatusCodes.Status200OK)]
-		[HttpGet("Get List of Question by section")]
-		[OpenApiOperation("Get list of all question by section", "")]
+		[HttpGet("get-question-by-section-id")]
+		[OpenApiOperation("get-question-by-section-id", "Get list of all question by section")]
 		public async Task<IActionResult> GetQuestionsBySection(Guid sectionId)
 		{
 			var questionSection = await _questionService.GetQuestionsBySection(sectionId);
@@ -93,8 +93,8 @@ namespace AMJNReportSystem.WebApi.Controllers
 
 
 		[ProducesResponseType(StatusCodes.Status200OK)]
-		[HttpGet("Get List of Question Option")]
-		[OpenApiOperation("Get list of all question option", "")]
+		[HttpGet("get-question-options-by-question-id")]
+		[OpenApiOperation("get-question-options-by-question-id", "Get list of all question option")]
 		public async Task<IActionResult> GetQuestionOptions(Guid questionId)
 		{
 			var questionOption = await _questionService.GetQuestionOptions(questionId);
