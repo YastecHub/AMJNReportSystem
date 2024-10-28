dfa1c63ab4a6fcebeb281c4d3ee69f7b59429597
Abdulhamiid Oyebo <oyeboharemu@yahoo.com>
2024-10-28 10:38:55 -0700

    Updated the response data

---
+++ Infrastructure/AMJNReportSystem.Persistence/Migrations/20241028170639_Update Report response.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AMJNReportSystem.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateReportresponse : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ReportResponses_ReportSubmissions_ReportSubmissionId",
+                table: "ReportResponses");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ReportSubmissionId",
+                table: "ReportResponses",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ReportResponses_ReportSubmissions_ReportSubmissionId",
+                table: "ReportResponses",
+                column: "ReportSubmissionId",
+                principalTable: "ReportSubmissions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ReportResponses_ReportSubmissions_ReportSubmissionId",
+                table: "ReportResponses");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ReportSubmissionId",
+                table: "ReportResponses",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ReportResponses_ReportSubmissions_ReportSubmissionId",
+                table: "ReportResponses",
+                column: "ReportSubmissionId",
+                principalTable: "ReportSubmissions",
+                principalColumn: "Id");
+        }
+    }
+}
